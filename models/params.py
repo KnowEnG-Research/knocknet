f8e3c75aceae87506659a0f3b02c7d4d49567187
root <root@ip-172-31-12-195.us-east-2.compute.internal>
2018-05-15 16:58:44 +0000

    add module strings to params

--- models/params.py
+++ models/params.py
@@ -1,9 +1,13 @@
+"""define parameters"""
 from argparse import ArgumentParser
+import os
+import yaml
 
 DEFAULT_CHKPT_DIR = 'chkpts'
 DEFAULT_LOG_DIR = 'logs'
 DEFAULT_BATCH_SIZE = 128
 def add_global_params(parser):
+    """define global parameters"""
     parser.add_argument('--chkpt_dir', default=DEFAULT_CHKPT_DIR, type=str,
                         help='Directory for saved model')
     parser.add_argument('--log_dir', default=DEFAULT_LOG_DIR, type=str,
@@ -16,6 +20,7 @@ def add_global_params(parser):
 DEFAULT_DATA_MODE = 'all'
 DEFAULT_DATA_DIR = './'
 def add_data_params(parser):
+    """define data parameters"""
     parser.add_argument('--data_dir', default=DEFAULT_DATA_DIR, type=str,
                         help='Directory storing input data')
     parser.add_argument('--data_serialized', dest='data_serialized', default=False,
@@ -32,23 +37,25 @@ DEFAULT_STEPS_PER_SAVE = 50000
 DEFAULT_OPTIMIZER_STR = "Adam"
 DEFAULT_LEARNING_RATE = "0.0001"
 def add_train_params(parser):
+    """define training parameters"""
     parser.add_argument('--train_max_steps', type=int, default=DEFAULT_MAX_STEPS,
                         help='Number of steps to run trainer')
     parser.add_argument('--train_steps_per_save', type=int, default=DEFAULT_STEPS_PER_SAVE,
                         help='Number of steps to run trainer before saving')
     parser.add_argument('--train_optimizer_str', type=str, default=DEFAULT_OPTIMIZER_STR,
                         help='Optimizer function')
-    parser.add_argument('--training', dest='is_training', default=False,
+    parser.add_argument('--training', dest='training', default=False,
                         action='store_true', help='Mark if training model')
     parser.add_argument('--train_learning_rate', type=float, default=DEFAULT_LEARNING_RATE,
                         help='Initial learning rate')
     return parser
 
-
+DEFAULT_KL_SPARCITY = 0.2
 def add_regularization_params(parser):
+    """define regularization parameters"""
     parser.add_argument('--reg_do_keep_prob', type=float, default=1.0,
                         help='Keep probability for training dropout')
-    parser.add_argument('--reg_kl_sparsity', type=float, default=0.2,
+    parser.add_argument('--reg_kl_sparsity', type=float, default=DEFAULT_KL_SPARCITY,
                         help='Probability for activation of neuron')
     parser.add_argument('--reg_kl_scale', type=float, default=0,
                         help='Regulariztion scalar for KL activation loss')
@@ -62,6 +69,7 @@ def add_regularization_params(parser):
 DEFAULT_ACTV_STR = 'sigmoid'
 DEFAULT_CONV_DEPTH = 0
 def add_conv_params(parser):
+    """define convolutional parameters"""
     parser.add_argument('--conv_depth', type=int, default=DEFAULT_CONV_DEPTH,
                         help='Depth of convolutional_layer')
     parser.add_argument('--conv_actv_str', type=str, default=DEFAULT_ACTV_STR,
@@ -76,6 +84,7 @@ def add_conv_params(parser):
 DEFAULT_DIM_STR = '5,5'
 DEFAULT_RES_BLOCK_SIZE = 0
 def add_fcs_params(parser):
+    """define fully connected layer parameters"""
     parser.add_argument('--fcs_dimension_str', type=str, default=DEFAULT_DIM_STR,
                         help='Hidden layers of the model')
     parser.add_argument('--fcs_actv_str', type=str, default=DEFAULT_ACTV_STR,
@@ -89,6 +98,7 @@ def add_fcs_params(parser):
 
 DEFAULT_NCLASSES = 50
 def add_outlayer_params(parser):
+    """define output parameters"""
     parser.add_argument('--out_actv_str', type=str, default=DEFAULT_ACTV_STR,
                         help='Activiation function')
     parser.add_argument('--out_label_count', type=int, default=DEFAULT_NCLASSES,
@@ -99,6 +109,7 @@ def add_outlayer_params(parser):
 DEFAULT_EVAL_MODE = 'summary'
 DEFAULT_EVAL_INTERVAL = 120
 def add_eval_params(parser):
+    """define evaluation parameters"""
     parser.add_argument('--eval_run_mode', type=str, default=DEFAULT_EVAL_MODE,
                         help='Mode: "summary", "verbose"')
     parser.add_argument('--eval_interval_secs', type=int, default=DEFAULT_EVAL_INTERVAL,
@@ -107,6 +118,7 @@ def add_eval_params(parser):
 
 
 def add_trainer_args(parser):
+    """merge parameters for training"""
     group1 = parser.add_argument_group('global parameters')
     group1 = add_global_params(group1)
     group2 = parser.add_argument_group('data parameters')
@@ -125,6 +137,7 @@ def add_trainer_args(parser):
 
 
 def add_evaluater_args(parser):
+    """merge parameters for evaluation"""
     group1 = parser.add_argument_group('global parameters')
     group1 = add_global_params(group1)
     group2 = parser.add_argument_group('data parameters')
@@ -139,9 +152,23 @@ def add_evaluater_args(parser):
     group8 = add_eval_params(group8)
     return parser
 
-
 def default_param_dict():
+    """create param_dict with defaults"""
     parser = ArgumentParser()
     parser = add_trainer_args(parser)
     param_dict = vars(parser.parse_args())
     return param_dict
+
+
+def param_dict_test():
+    """test param module"""
+    parser = ArgumentParser()
+    parser = add_trainer_args(parser)
+    param_dict = vars(parser.parse_args())
+    param_yml = os.path.join(param_dict["log_dir"], 'params.yml')
+    with open(param_yml, 'w') as outfile:
+        yaml.dump(param_dict, outfile, default_flow_style=False)
+
+
+if __name__ == '__main__':
+    param_dict_test()
