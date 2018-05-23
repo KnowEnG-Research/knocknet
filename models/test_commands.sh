f08cf8069415e9cfd7a5efaf3c8fcc601d02dbeb
root <root@ip-172-31-12-195.us-east-2.compute.internal>
2018-05-23 01:47:59 +0000

    add serialized example

--- models/test_commands.sh
+++ models/test_commands.sh
@@ -1,6 +1,6 @@
 # wget https://raw.github.com/lehmannro/assert.sh/v1.1/assert.sh
-LOAD_TESTS=0
-MODEL_TESTS=0
+LOAD_TESTS=1
+MODEL_TESTS=1
 TRAIN_TESTS=1
 TEST_TESTS=1
 
@@ -10,6 +10,8 @@ LOGDIR="/mnt/knowdnn_hdd/tfuser/logs/tests/"
 mkdir -p $LOGDIR
 TESTCT=1
 
+#python3 serialize_features.py /home/tfuser/data/test_tsv/ /home/tfuser/data/test_tfrecord/ --label_col 7
+
 # 1: check param dict
 CMD="python3 params.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
     --log_dir $LOGDIR &> $LOGDIR/$TESTCT.log"
@@ -18,7 +20,7 @@ CMD="python3 params.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 
 
 # 2: check basic plain text shuffle load data
 CMD="python3 load_data.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-    --training --data_mode all --batch_size 6 --train_max_steps 1 &> $LOGDIR/$TESTCT.log"
+    --training --data_mode all --data_batch_size 6 --train_max_steps 1 &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
 ((LOAD_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
 
@@ -27,7 +29,7 @@ for SHUF in ""; do
     for DMODE in all diff exp_only; do
         for STEPS in 1; do
             CMD="python3 load_data.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-            $SHUF --data_mode $DMODE --batch_size 6 --train_max_steps $STEPS &> $LOGDIR/$TESTCT.log"
+            $SHUF --data_mode $DMODE --data_batch_size 6 --train_max_steps $STEPS &> $LOGDIR/$TESTCT.log"
             # echo -e "\n****$TESTCT: $CMD"
             ((LOAD_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
         done;
@@ -39,7 +41,7 @@ for SHUF in --training ""; do
     for DMODE in exp_only; do
         for STEPS in 4; do
             CMD="python3 load_data.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-            $SHUF --data_mode $DMODE --batch_size 6 --train_max_steps $STEPS &> $LOGDIR/$TESTCT.log"
+            $SHUF --data_mode $DMODE --data_batch_size 6 --train_max_steps $STEPS &> $LOGDIR/$TESTCT.log"
             # echo -e "\n****$TESTCT: $CMD"
             ((LOAD_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
         done;
@@ -48,28 +50,36 @@ done
 
 # 8: check basic model construction
 CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-     --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+     --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
      &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
 ((MODEL_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
 
 # 9: check input layer batch norm
 CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-     --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+     --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
      --data_batch_norm &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
 ((MODEL_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
 
 # 10: check normal conv layer options
 CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-     --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+     --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
      --conv_actv_str relu --conv_batch_norm --conv_depth 3 &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
 ((MODEL_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
 
+# 11: check normal conv layer options
+CMD="python3 models.py --data_dir /home/tfuser/data/test_tfrecord/ --data_serialized --out_label_count 10 \
+     --data_batch_size 4 --data_mode all  --train_max_steps 1 --reg_do_keep_prob 0.7 \
+     --conv_actv_str relu --conv_batch_norm --conv_depth 3 &> $LOGDIR/$TESTCT.log"
+# echo -e "\n****$TESTCT: $CMD"
+((MODEL_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
+
+
 # 11: check conv_gene_pair_layer options
 CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-     --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+     --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
      --conv_batch_norm --conv_gene_pair --conv_depth 3 &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
 ((MODEL_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
@@ -78,7 +88,7 @@ CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 
 for CONVD in 0 3; do
     for DIMS in 0 5 7,5; do
         CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-             --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+             --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
              --conv_depth $CONVD --fcs_dimension_str $DIMS --fcs_actv_str relu --fcs_batch_norm \
              &> $LOGDIR/$TESTCT.log"
         # echo -e "\n****$TESTCT: $CMD"
@@ -88,7 +98,7 @@ done
 
 # 18: check residual blocks
 CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-    --batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
+    --data_batch_size 4 --data_mode all  --train_max_steps 1 --training --reg_do_keep_prob 0.7 \
     --conv_depth 3 --fcs_dimension_str 8,8,8,8,8,8 --fcs_res_block_size 2 --fcs_batch_norm \
     &> $LOGDIR/$TESTCT.log"
 # echo -e "\n****$TESTCT: $CMD"
@@ -98,8 +108,8 @@ CMD="python3 models.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 
 rm -r "$LOGDIR/chkptdir_$TESTCT"
 MYTRAINNUM=$TESTCT
 CMD="python3 train.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-    --chkpt_dir $LOGDIR/chkptdir_$TESTCT --train_save_summ_secs 4 --train_save_ckpt_secs 8\
-    --batch_size 8 --data_mode all --out_actv_str None --train_max_steps 5000 \
+    --train_chkpt_dir $LOGDIR/chkptdir_$TESTCT --train_save_summ_secs 4 --train_save_ckpt_secs 8\
+    --data_batch_size 8 --data_mode all --out_actv_str None --train_max_steps 5000 \
     --training --reg_do_keep_prob 0.7 --train_learning_rate 0.1  \
     --fcs_dimension_str 0 --data_batch_norm \
     &> $LOGDIR/$TESTCT.log"
@@ -110,8 +120,8 @@ CMD="python3 train.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 1
 for RM in stats preds probs; do
     rm -r "$LOGDIR/chkptdir_$TESTCT"
     CMD="python3 eval.py --data_dir /home/tfuser/data/test_tsv/ --data_mode all \
-        --chkpt_dir $LOGDIR/chkptdir_$MYTRAINNUM --log_dir $LOGDIR/chkptdir_$MYTRAINNUM/log_$TESTCT \
-        --batch_size 6 --eval_run_mode $RM \
+        --train_chkpt_dir $LOGDIR/chkptdir_$MYTRAINNUM --log_dir $LOGDIR/chkptdir_$MYTRAINNUM/log_$TESTCT \
+        --data_batch_size 6 --eval_run_mode $RM \
         &> $LOGDIR/$TESTCT.log"
     # echo -e "\n****$TESTCT: $CMD"
     ((TEST_TESTS)) && assert_raises "$CMD" && ((TESTCT=TESTCT+1))
@@ -121,8 +131,8 @@ done
 for LF in reg_l1_scale reg_l2_scale reg_kl_scale; do
     rm -r "$LOGDIR/chkptdir_$TESTCT"
     CMD="python3 train.py --data_dir /home/tfuser/data/test_tsv/ --out_label_count 10 \
-        --chkpt_dir $LOGDIR/chkptdir_$TESTCT --train_save_summ_secs 2 \
-        --batch_size 8 --data_mode all  --train_max_steps 2000 --training --reg_do_keep_prob 0.7 \
+        --train_chkpt_dir $LOGDIR/chkptdir_$TESTCT --train_save_summ_secs 2 \
+        --data_batch_size 8 --data_mode all  --train_max_steps 2000 --training --reg_do_keep_prob 0.7 \
         --conv_depth 3 --fcs_dimension_str 5,5 --$LF 2 --data_batch_norm \
         &> $LOGDIR/$TESTCT.log"
     # echo -e "\n****$TESTCT: $CMD"
